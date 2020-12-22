d69d30373c7de6debf117a5e20f5938f8b860ebb
Renato Pavlic <renato.poslovni@gmail.com>
2020-12-22 21:19:55 +0100

    upload image functionality

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { db, auth } from "./firebase";
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
 import { Button, Input } from "@material-ui/core";
+import ImageUpload from "./ImageUpload";
 
 
 // Material UI styling
@@ -105,7 +106,7 @@ function App() {
 
   useEffect(() => {
     // Database data
-    db.collection("posts").onSnapshot( snapshot => {
+    db.collection("posts").orderBy("timestamp", "desc" ).onSnapshot( snapshot => {
       setPosts( snapshot.docs.map( doc => ({
         id: doc.id,
         post: doc.data()
@@ -116,6 +117,12 @@ function App() {
 
   return (
     <div className="app">
+
+      {user?.displayName ? (
+      <ImageUpload username={user.displayName}/>
+      ) : (
+      <h3>Sorry you need to login to upload</h3>
+      )}
 
       <Modal
         open={open}
